bf670b9cdc60daf2b28f8fc06a2e52b61caaddd5
Frederico Bernardo Rodrigues <fred@MacBook-Air-de-Frederico.local>
2025-12-06 12:24:56 -0300

    Fix app package and sqlite config

--- app/database.py
+++ app/database.py
@@ -5,7 +5,11 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/judicial_db")
 
-engine = create_engine(DATABASE_URL)
+connect_args = {}
+if DATABASE_URL.startswith("sqlite"):
+    connect_args["check_same_thread"] = False
+
+engine = create_engine(DATABASE_URL, connect_args=connect_args)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
